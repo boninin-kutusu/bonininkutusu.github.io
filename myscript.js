5ad7ac7b7734a11b4af82cabeedb84e53184ab71
Busta <baranusta@sabanciuniv.edu>
2018-09-10 20:28:54 +0300

    carousel and font family changed

--- myscript.js
+++ myscript.js
@@ -1,29 +1,41 @@
 var $window = $(window)
 const endScroll=200
 var logo
-var offset
+var centerOffsetLogo
 var mostleft
 var initialHeight
 var initialBottomMargin
 
-$window.ready(function(){
-    logo = $("#logo")
-    offset = logo.offset()
-    initialHeight = logo.width()
-    let mrg = logo.css('margin-bottom')
-    initialBottomMargin = mrg.substring(0,mrg.indexOf('px'))
+var resetPositions = function(){
     mostleft = $(".header-bar").offset().left
+    centerLogoLeft = window.innerWidth / 2 - logo.width()/2
 
     let navbar = $("#navigation-bar")
     navbar.offset({left:$(".header-bar").width()/2 + mostleft - navbar.width() / 2})
-    console.log(navbar.width())
-    console.log(window.innerWidth)
+
+    
+    var scrollTop = $window.scrollTop();
+    scroll= Math.max(endScroll - scrollTop, 0)/endScroll
+    logo.offset({left: (centerLogoLeft-mostleft) * scroll + mostleft })
+    logo.width(initialHeight * (1 - (1 - scroll) * 0.6))
+}
+$window.ready(function(){
+    logo = $("#logo")
+    initialHeight = logo.width()
+    
+    let mrg = logo.css('margin-bottom')
+    initialBottomMargin = mrg.substring(0,mrg.indexOf('px'))
+    resetPositions()
 })
+
+$( window ).resize(function() {
+    resetPositions()
+});
 
 $window.scroll(function() {
     var scrollTop = $window.scrollTop();
     scroll= Math.max(endScroll - scrollTop, 0)/endScroll
-    logo.offset({left: (offset.left-mostleft) * scroll + mostleft })
+    logo.offset({left: (centerLogoLeft-mostleft) * scroll + mostleft })
     logo.width(initialHeight * (1 - (1 - scroll) * 0.6))
     logo.css('margin-bottom',Math.max(initialBottomMargin * (1 - (1 - scroll)), 10) + 'px')
 });  
